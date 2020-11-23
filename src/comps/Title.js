52565ac438399efe03a2b1d9293f7f604a24d1c2
Samrat De <desamrat31@gmail.com>
2020-11-23 23:38:06 +0530

    Added the Headings

---
+++ src/comps/Title.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Title = () => {
+	return (
+		<div className='title'>
+			<h1>Firegram</h1>
+			<h2>Your Pictures</h2>
+			<p>
+				These are all the pictures which you have uploaded till now.
+				Scroll down to view more.
+			</p>
+		</div>
+	)
+}
+
+export default Title
