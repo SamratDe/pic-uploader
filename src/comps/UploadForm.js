7817c231a3001ee9e7af9e291f920194aca75805
Samrat De <desamrat31@gmail.com>
2020-11-24 13:01:19 +0530

    added firebase storage hook

---
+++ src/comps/UploadForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+const UploadForm = () => {
+	const [file, setFile] = useState(null)
+	const [error, setError] = useState(null)
+	const types = ['image/png', 'image/jpeg']
+
+	const changeHandler = (e) => {
+		let selected = e.target.files[0]
+		if (selected && types.includes(selected.type)) {
+			setFile(selected)
+			setError('')
+		} else {
+			setFile(null)
+			setError('Please select an Image file (png or jpeg)')
+		}
+	}
+
+	return (
+		<form>
+			<label>
+				<input type='file' name='' id='' onChange={changeHandler} />
+				<span>+</span>
+			</label>
+			<div className='output'>
+				{error && <div className='error'>{error}</div>}
+				{file && <div>{file.name}</div>}
+			</div>
+		</form>
+	)
+}
+
+export default UploadForm
